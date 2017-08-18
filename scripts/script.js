a95c6a64c5d53d46d5904725e9d79770c92b10ed
Greg Hazelton <haze0028@algonquinlive.com>
2017-08-18 01:13:00 -0400

    Fixed js error

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,22 @@
-document.getElementById("gButton").onclick = guests;
-	document.getElementById("sButton").onclick = speakers;
+//REMOVED FORM FROM PAGE, SO NO JS REQUIRED
 
-/****************************
-	GUESTS/SPEAKERS
-*****************************/
-function guests(){
-	document.querySelector("#form1").style.display = "block";
-	document.querySelector("#form2").style.display = "none";
-	this.style.background = "black";
-	document.getElementById("sButton").style.background = "grey";
-	
-}
-
-function speakers(){
-	document.querySelector("#form2").style.display = "block";	
-	document.querySelector("#form1").style.display = "none";
-	this.style.background = "black";
-	document.getElementById("gButton").style.background = "grey";
-}
+//document.getElementById("gButton").onclick = guests;
+//	document.getElementById("sButton").onclick = speakers;
+//
+///****************************
+//	GUESTS/SPEAKERS
+//*****************************/
+//function guests(){
+//	document.querySelector("#form1").style.display = "block";
+//	document.querySelector("#form2").style.display = "none";
+//	this.style.background = "black";
+//	document.getElementById("sButton").style.background = "grey";
+//	
+//}
+//
+//function speakers(){
+//	document.querySelector("#form2").style.display = "block";	
+//	document.querySelector("#form1").style.display = "none";
+//	this.style.background = "black";
+//	document.getElementById("gButton").style.background = "grey";
+//}
